7c928ea6d73fff743f8c1eaefb4cfa3bf8b8acbd
sjan <sjan>
2024-06-30 16:09:42 +0900

    채팅룸 구축

--- src/main/java/com/example/websocket/WebSocketHandler.java
+++ src/main/java/com/example/websocket/WebSocketHandler.java
@@ -1,5 +1,10 @@
 package com.example.websocket;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import org.json.JSONObject;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -8,20 +13,33 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
+	Map<WebSocketSession, String> user = new HashMap<WebSocketSession, String>();
+	
 	/* 클라이언트로부터 메시지 수신시 동작 */
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		JSONObject receiveMessage = new JSONObject(message.getPayload());
+		if(receiveMessage.get("type").equals("login")) {
+			user.put(session, (String) receiveMessage.get("id"));
+		}
+		Iterator<WebSocketSession> keys = user.keySet().iterator();
+		while( keys.hasNext() ){
+			WebSocketSession strKey = keys.next();
+			if(strKey != session) {
+				strKey.sendMessage(new TextMessage(message.getPayload()));	
+			}
+		}
+		System.out.println(receiveMessage);
 	}
 	
 	/* 클라이언트가 소켓 연결시 동작 */
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		System.out.println("연결됨");
 	}
 	
 	/* 클라이언트가 소켓 종료시 동작 */
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		System.out.println("연결안됨");
+		user.remove(session);
 	}
 }
